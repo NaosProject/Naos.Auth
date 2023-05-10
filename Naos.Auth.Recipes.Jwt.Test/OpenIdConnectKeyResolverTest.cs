53920173e57bea425936f7ee4be1e504354ca85f
Lawson Caudill <1660562+wlscaudill@users.noreply.github.com>
2023-05-10 00:16:57 -0400

    Added support logic to provide .NET framework with a self contained implementation of IssuerSigningKeyResolver for use with TokenValidationParameters.

---
+++ Naos.Auth.Recipes.Jwt.Test/OpenIdConnectKeyResolverTest.cs
@@ -0,0 +1,52 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="OpenIdConnectKeyResolverTest.cs" company="Naos Project">
+//    Copyright (c) Naos Project 2019. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Naos.Auth.Recipes.Jwt.Test
+{
+    using System;
+    using System.IdentityModel.Tokens;
+    using System.Security.Principal;
+    using OBeautifulCode.Assertion.Recipes;
+
+    using Xunit;
+
+    public static class OpenIdConnectKeyResolverTest
+    {
+        [Fact(Skip = "Debug only.")]
+        public static void TestWithTokenValidationHandler()
+        {
+            var authToken = "X.Y.Z";
+
+            var audiences = new[]
+                            {
+                                "https://web.api.myCompany.com",
+                            };
+
+            var issuer = "https://myCompany.us.auth0.com/";
+
+            using (var resolver = new OpenIdConnectKeyResolver(issuer, TimeSpan.Zero))
+            {
+                var tokenHandler = new JwtSecurityTokenHandler();
+
+                var validationParameters = new TokenValidationParameters()
+                                           {
+                                               ValidateLifetime = true,
+                                               ValidateAudience = true,
+                                               ValidateIssuer = true,
+                                               ValidateActor = true,
+                                               ValidateIssuerSigningKey = true,
+                                               ValidAudiences = audiences,
+                                               ValidIssuer = issuer,
+                                               IssuerSigningKeyResolver = resolver.GetSigningKey,
+                                           };
+
+                IPrincipal principal = tokenHandler.ValidateToken(authToken, validationParameters, out var validatedToken);
+                validatedToken.MustForTest().NotBeNull();
+                principal.MustForTest().NotBeNull();
+            }
+        }
+    }
+}
