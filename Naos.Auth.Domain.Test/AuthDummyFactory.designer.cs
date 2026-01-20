86e9b3b17c14cb3147736071367a99f6dcfdc160
Suraj Gupta <SurajGupta@users.noreply.github.com>
2026-01-20 15:16:36 -0500

    added OAuth2Initiation object

--- Naos.Auth.Domain.Test/AuthDummyFactory.designer.cs
+++ Naos.Auth.Domain.Test/AuthDummyFactory.designer.cs
@@ -42,6 +42,11 @@ namespace Naos.Auth.Domain.Test
                                  A.Dummy<string>()));
 
             AutoFixtureBackedDummyFactory.AddDummyCreator(
+                () => new OAuth2Initiation(
+                                 A.Dummy<string>(),
+                                 A.Dummy<string>()));
+
+            AutoFixtureBackedDummyFactory.AddDummyCreator(
                 () => new TokenInfo(
                                  A.Dummy<string>(),
                                  A.Dummy<UtcDateTimeRangeInclusive>()));
---
+++ Naos.Auth.Domain/Models/Classes/OAuth2Initiation.cs
@@ -0,0 +1,54 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="OAuth2Initiation.cs" company="Naos Project">
+//    Copyright (c) Naos Project 2019. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Naos.Auth.Domain
+{
+    using System.Diagnostics.CodeAnalysis;
+    using Naos.CodeAnalysis.Recipes;
+    using OBeautifulCode.Assertion.Recipes;
+    using OBeautifulCode.Type;
+
+    /// <summary>
+    /// Represents the initial data required to begin an OAuth 2.0 authorization code flow,
+    /// including the authorization URL and state for CSRF validation.
+    /// </summary>
+    public partial class OAuth2Initiation : IModelViaCodeGen
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OAuth2Initiation"/> class.
+        /// </summary>
+        /// <param name="authorizationUrl">The URL to redirect the user to for authorization.</param>
+        /// <param name="csrfValidationState">
+        /// The state value to validate against CSRF (cross-site request forgery) attacks
+        /// when handling the authorization callback.
+        /// </param>
+        [SuppressMessage("Microsoft.Design", "CA1054:UriParametersShouldNotBeStrings", MessageId = "0#", Justification = "Prefer URIs as strings.")]
+        [SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "csrf", Justification = NaosSuppressBecause.CA1704_IdentifiersShouldBeSpelledCorrectly_SpellingIsCorrectInContextOfTheDomain)]
+        public OAuth2Initiation(
+            string authorizationUrl,
+            string csrfValidationState)
+        {
+            new { authorizationUrl }.AsArg().Must().NotBeNullNorWhiteSpace();
+            new { csrfValidationState }.AsArg().Must().NotBeNullNorWhiteSpace();
+
+            this.AuthorizationUrl = authorizationUrl;
+            this.CsrfValidationState = csrfValidationState;
+        }
+
+        /// <summary>
+        /// Gets the URL to redirect the user to for authorization.
+        /// </summary>
+        [SuppressMessage("Microsoft.Design", "CA1056:UriPropertiesShouldNotBeStrings", Justification = "Prefer URIs as strings.")]
+        public string AuthorizationUrl { get; private set; }
+
+        /// <summary>
+        /// Gets the state value to validate against CSRF (cross-site request forgery) attacks
+        /// when handling the authorization callback.
+        /// </summary>
+        [SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "Csrf", Justification = NaosSuppressBecause.CA1704_IdentifiersShouldBeSpelledCorrectly_SpellingIsCorrectInContextOfTheDomain)]
+        public string CsrfValidationState { get; private set; }
+    }
+}
